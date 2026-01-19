be0466359ce7a31797f9714511e0dba10a3664f0
Luís Felipe Silva <felipepereiracript@gmail.com>
2026-01-19 00:11:12 -0300

    add Float class to test casting

--- ocp/src/app/whatever/Types.java
+++ ocp/src/app/whatever/Types.java
@@ -1,14 +1,28 @@
 package app.whatever;
 
-public class Types {
+class Chars {
   char myChar = 'a';
   static String myString = "ABC";
 
+  public static void execute() {
+    Chars type = new Chars();
+    type.myChar = 'b';
+    Chars.myString = "CBA";
+
+    System.out.println(myString + " " + type.myChar);
+  }
+}
+
+class Floats { 
+  /**
+   * The precision of a floating point value indicates how many digits the value can have after the decimal point. 
+   * The precision of float is only 6-7 decimal digits, while double variables have a precision of about 16 digits.
+   */
+  static float x = (float) 5.5d;
+}
+
+public class Types {
   public static void main(String[] args) {
-  Types type = new Types();
-  type.myChar = 'b';
-  Types.myString = "CBA";
-  
-  System.out.println(myString + " " + type.myChar);
+    System.out.println(Floats.x);
   }
 }
